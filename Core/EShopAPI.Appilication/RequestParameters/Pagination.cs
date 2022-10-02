2e08631832efcf902959c2b0f2960e18de244d5d
Aykhan <axundovayxan9@gmail.com>
2022-10-02 23:52:49 +0400

    Updated

--- Core/EShopAPI.Appilication/RequestParameters/Pagination.cs
+++ Core/EShopAPI.Appilication/RequestParameters/Pagination.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace EShopAPI.Appilication.RequestParameters
 {
     public record Pagination
--- Infrastructure/EShopAPI.Infrastructure/Filters/ValidationFilter.cs
+++ Infrastructure/EShopAPI.Infrastructure/Filters/ValidationFilter.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 namespace EShopAPI.Infrastructure.Filters
 {
     public class ValidationFilter : IAsyncActionFilter
